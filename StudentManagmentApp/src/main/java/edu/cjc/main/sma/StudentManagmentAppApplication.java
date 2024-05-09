07bb0f64a61dfc086377a8c06a663cbbadb62c70
cjcnkolhe <103464730+cjcnkolhe@users.noreply.github.com>
2024-05-09 12:22:05 +0530

    Update StudentManagmentAppApplication.java
    c daded

--- StudentManagmentApp/src/main/java/edu/cjc/main/sma/StudentManagmentAppApplication.java
+++ StudentManagmentApp/src/main/java/edu/cjc/main/sma/StudentManagmentAppApplication.java
@@ -9,6 +9,7 @@ public class StudentManagmentAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagmentAppApplication.class, args);
 		int b;
+		int c;
 	}
 
 }
